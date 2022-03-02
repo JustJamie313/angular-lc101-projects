cd33389695a0eadb36bfa7ffce7c3dfcf87ed241
KCCoderGirl <dontlaughatme@yahoo.com>
2022-03-02 09:42:24 -0600

    lesson-2 ngFor examples complete

--- lesson2/examples/ngfor-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngfor-practice/src/app/chores/chores.component.ts
@@ -6,12 +6,37 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./chores.component.css']
 })
 export class ChoresComponent implements OnInit {
-   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
-   todoTitles = ["Yesterday's Chores", "Today's Chores", "Tomorrow's Chores"];
+   chores = [
+      {status:'open',task:'Complete Launchcode work'},
+      {status:'open',task:'Clean Bathroom'},
+      {status:'complete',task:'Wake up'}
+   ];
+   todoTitles = ["Completed", "To-Do List"];
 
+   choreCompleted(chore){
+      chore.status = 'complete';
+   }
+   processCompleted(chore){
+      let archive = window.confirm('Move to Arcive?');
+      if(!archive){
+         let todo = window.confirm('Move to To-Do List?');
+         if(todo){
+            console.log('here');
+            chore.status = 'open';
+            return;
+         }
+      }
+      chore.status = '';
+      return;
+   }
+   addTask(){
+      let t = window.prompt('Enter task:','Enter task');
+      this.chores.push({status:'open',task: t});
+   }
    constructor() { }
 
    ngOnInit() {
-   }
 
+   }
 }
+
